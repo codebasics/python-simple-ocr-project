e16082988620fe05bbf61fe6ca19e8c5bf528e8c
d_p_beladiya <darshan@atliq.com>
2021-02-16 18:18:19 +0530

    RFC: Cleaned code

--- api/extractor/extract.py
+++ api/extractor/extract.py
@@ -1,33 +1,12 @@
 import cv2
 from flask import current_app as app
-from extractor import format2
-from extractor import format3
-from extractor import format4
 from extractor import format1
 from image_utils.image_to_string import from_image_to_string
 import numpy as np
 
 functions = {
-    'format1': format1,
-    'format2': format2,
-    'format3': format3,
-    'format4': format4
+    'format1': format1
 }
-
-
-def clarify_text(img):
-    img2 = img.copy().astype('uint8')
-    disp_img = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY).astype('uint8')
-    # blur = cv2.GaussianBlur(disp_img, (3, 3), 0).astype('uint8')
-    ret3, th3 = cv2.threshold(disp_img, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU) # noqa
-    disp_img = cv2.medianBlur(th3, 3)
-    disp_img = cv2.resize(disp_img, None, fx=5, fy=5, interpolation=cv2.INTER_CUBIC) # noqa
-    kernel = np.ones((3, 3), np.uint8)
-    dilated = cv2.dilate(th3, kernel, iterations=1)
-    eroded = cv2.erode(dilated, kernel, iterations=1)
-    dilated = cv2.dilate(eroded, kernel, iterations=1)
-    eroded = cv2.erode(dilated, kernel, iterations=1)
-    return eroded
 
 
 def get_information(img_list, file_format):
--- api/image_utils/image_to_string.py
+++ api/image_utils/image_to_string.py
@@ -1,5 +1,4 @@
 import pytesseract
-from image_utils.segment import get_lines
 
 # not working
 config_str = (
@@ -21,20 +20,5 @@ def from_image_to_string(img_list):
         text = str(pytesseract.image_to_string(img, lang='eng'))
         text = text.replace('-\n', '')
         text_list.append(text)
-    text = "\n".join(text_list)
-    return text
-
-
-def from_lines_to_string(img_list):
-    """
-    converts the list of images into lines and extracts text from lines
-    """
-    text_list = []
-    for img in img_list:
-        lines = get_lines(img)
-        for line in lines:
-            text = str(pytesseract.image_to_string(line, lang='eng'))
-            text = text.replace('-\n', '')
-            text_list.append(text)
     text = "\n".join(text_list)
     return text
